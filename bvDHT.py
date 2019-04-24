96af55c4aa282b0e201c2ada5c5d18274588f787
tdfree84 <tyler.freese98@gmail.com>
2019-04-24 15:57:38 -0500

    Added thread to wait for clients.

--- bvDHT.py
+++ bvDHT.py
@@ -9,7 +9,7 @@ import hash_functions
 
 def handlePeer(peerInfo):
     #handle a new client that connects
-    print("You have connected and are being handled")
+    print("I have connected with someone.")
     peerConn, peerAddr = peerInfo
     conMsg = peerConn.hash_functions.recvAll(peerConn, 3)
     print(conMsg)
@@ -17,6 +17,11 @@ def handlePeer(peerInfo):
     print(peerIP + ":" + peerPort)
     #fingerTable = {}
     #keySpaceRanges = 2**160/5
+
+def waitForPeerConnections(listener):
+    while running:
+        peerInfo = listener.accept()
+        threading.Thread(target=handlePeer, args = (peerInfo,), daemon=True).start()
 
 ##port = 54545
 listener = socket(AF_INET, SOCK_STREAM)
@@ -32,29 +37,27 @@ if len(sys.argv) == 1:
     fingerTable = {}
     keySpaceRanges = 2**160/5
     #set up our own thread to start listening for clients
-    while running:
-        threading.Thread(target=handlePeer, args = (listener.accept(),), daemon=True).start()
+    threading.Thread(target=waitForPeerConnections, args = (listener,), daemon=True).start()
+
     print("This is a the seed client")
     #this will be for the initial person connecting
 elif len(sys.argv) == 3:
     #this is for any peer trying to connect to another peer
-    print("This is a peer connecting")
     #set up our own thread to start listening for clients
-    while running:
-        threading.Thread(target=handlePeer, args = (listener.accept(),), daemon=True).start()
+    threading.Thread(target=waitForPeerConnections, args = (listener,), daemon=True).start()
 
+    # Continue connecting to peer the user chose
     peerIP = sys.argv[1]
     peerPort = int(sys.argv[2])
-
     peerConn = socket(AF_INET, SOCK_STREAM)
     peerConn.connect( (peerIP, peerPort) )
 else:
     print("What you doing?")
 
-    #send the client we connected to our connection protocol
-    peerConn.send("CON".encode())
-    peerAddress = net_functions.getLocalIPAddress() + ":" + str(port)
-    peerConn.hash_functions.sendAddress(peerConn, peerAddress)
+#   #send the client we connected to our connection protocol
+#   peerConn.send("CON".encode())
+#   peerAddress = net_functions.getLocalIPAddress() + ":" + str(port)
+#   peerConn.hash_functions.sendAddress(peerConn, peerAddress)
 
 
 
