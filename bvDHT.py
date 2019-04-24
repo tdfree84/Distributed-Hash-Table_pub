96f01f09533f34b8cbf027a54db8fd6813a00d63
jackmford <fordjac@bvu.edu>
2019-04-24 15:20:18 -0500

    comm

--- bvDHT.py
+++ bvDHT.py
@@ -4,14 +4,17 @@ import sys
 import os
 import time
 import threading
+import net_functions
+import hash_functions
 
 def handlePeer(listener):
     while running:
         #handle a new client that connects
-        fingerTable = {}
-        keySpaceRanges = 2**160/5
+        print("You have connected and are being handled")
         peerInfo = listener.accept()
         peerConn, peerAddr = peerInfo
+        fingerTable = {}
+        keySpaceRanges = 2**160/5
 
 port = 54545
 listener = socket(AF_INET, SOCK_STREAM)
@@ -19,24 +22,27 @@ listener.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 listener.bind(('', port))
 listener.listen(32)
 
+running = True
 
 if len(sys.argv) == 1:
     fingerTable = {}
     keySpaceRanges = 2**160/5
+    while running:
     #set up our own thread to start listening for clients
-    threading.Thread(target=handlePeer, args = (listener,), daemon=True).start()
+        threading.Thread(target=handlePeer, args = (listener,), daemon=True).start()
     print("This is a the seed client")
     #this will be for the initial person connecting
-else if len(sys.argv) == 3:
+elif len(sys.argv) == 3:
     #this is for any peer trying to connect to another peer
     print("This is a peer connecting")
+    #set up our own thread to start listening for clients
+    threading.Thread(target=handlePeer, args = (listener,), daemon=True).start()
+
     peerIP = sys.argv[1]
     peerPort = int(sys.argv[2])
 
     peerConn = socket(AF_INET, SOCK_STREAM)
     peerConn.connect( (peerIP, peerPort) )
-    #set up our own thread to start listening for clients
-    threading.Thread(target=handlePeer, args = (listener,), daemon=True).start()
 
 
 
