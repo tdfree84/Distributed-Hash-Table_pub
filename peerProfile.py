c161d0e60798d67026869fce16d8a234015b2323
tdfree84 <tyler.freese98@gmail.com>
2019-04-26 14:13:16 -0500

    Added info on datastructuers to peerprofile.

--- peerProfile.py
+++ peerProfile.py
@@ -1,4 +1,15 @@
 class PeerProfile:
+    '''
+        Data structures:
+        
+        myAddress: tuple( getLocalIPAddress(), int(port) )
+
+        minHash: int( getHashIndex((getLocalIPAddress(), int(port))) )
+        maxHash: int( getHashIndex((getLocalIPAddress(), int(port))) - 1 )
+
+        fingerTable: { key = getHashIndex((getLocalIPAddress(), int(port))) : value = str(getLocalIPAddress():port), ... }
+
+    '''
 
     def __init__(self, _myAddr, _minHash, _maxHash, _fingerTable, _successor, _predecessor):
         self.minHash = _minHash
