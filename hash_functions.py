387515f7d90a7c35cec4511f3f4b063227167914
tdfree84 <tyler.freese98@gmail.com>
2020-02-08 08:16:04 -0600

    Added authors to hash functions

--- hash_functions.py
+++ hash_functions.py
@@ -10,7 +10,7 @@ import hashlib
 #########################
 #########################
 
-
+# Author: Dr. Nathan Backman
 # Returns an integer index into the hash-space for a node Address
 #  - addr is of the form ("ipAddress or hostname", portNumber)
 #    where the first item is a string and the second is an integer
@@ -20,4 +20,7 @@ def getHashIndex(addr):
 
 
 
-
+# Author: Tyler Freese
+# Get a hash of any value
+def getHash(key):
+    return int.from_bytes(hashlib.sha1(key).digest(), byteorder="big")
