6e9c3731a35e1b5da4da4826ccb9725aed54ace2
tdfree84 <tyler.freese98@gmail.com>
2020-02-08 10:27:37 -0600

    Added testing and true owner to dhti

--- tests/test_dhti.py
+++ tests/test_dhti.py
@@ -10,7 +10,6 @@ def new_dhti():
     yield cfg
     cfg.close_connection()
 
-
 def test_exists(new_dhti):
     cfg = new_dhti
 
@@ -19,3 +18,55 @@ def test_exists(new_dhti):
     exi = cfg.exists(key_that_doesnt_exist)
     assert exi == 'F'
 
+def test_insert(new_dhti):
+    cfg = new_dhti
+
+    key_for_insert = 'iamtestkey'
+    value_for_insert = 'testingtesting'
+
+    ins = cfg.insert(key_for_insert,value_for_insert)
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    exi = cfg.exists(key_for_insert)
+    assert ins == 'T'
+    assert exi == 'T'
+
+def test_remove(new_dhti):
+    cfg = new_dhti
+
+    key_for_insert = 'iamtestkey'
+    value_for_insert = 'testingtesting'
+
+    ins = cfg.insert(key_for_insert,value_for_insert)
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    exi = cfg.exists(key_for_insert)
+    assert ins == 'T'
+    assert exi == 'T'
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    rem = cfg.remove(key_for_insert)
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    exi = cfg.exists(key_for_insert)
+    assert exi == 'F'
+    assert rem == 'T'
+
+def test_get(new_dhti):
+    cfg = new_dhti
+
+    key_for_insert = 'iamtestkey'
+    value_for_insert = 'testingtesting'
+
+    ins = cfg.insert(key_for_insert,value_for_insert)
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    exi = cfg.exists(key_for_insert)
+    assert ins == 'T'
+    assert exi == 'T'
+
+    cfg.read_and_set_connection(IPPORTFILENAME)
+    get = cfg.get(key_for_insert)
+
+    assert get[0] == 'T'
+    assert get[1].decode() == value_for_insert
--- tfbvDHT/dhti.py
+++ tfbvDHT/dhti.py
@@ -16,6 +16,14 @@ class DHTInterface:
         else:
             return getHash(key)
 
+    # Make sure value is sendable
+    def prepVal(self, val):
+        if type(val) is str:
+            return val.encode()
+        else:
+            return val
+
+
     # Read ip/port from local file named IPPORT.txt
     def read_and_set_connection(self, file_name):
         IPPORT = None
@@ -25,59 +33,88 @@ class DHTInterface:
                 IPPORT = line.split(':')
             except:
                 raise("IPPORT file was not proper.")
+        self.peerIP = IPPORT[0]
+        self.peerPort = IPPORT[1]
         self.set_connection(IPPORT[0], int(IPPORT[1]))
 
     # Set connection
     def set_connection(self, peerIP, peerPort):
+        if self.conn is not None:
+            self.close_connection()
+
         self.conn = socket(AF_INET, SOCK_STREAM)
+        self.peerIP = peerIP
+        self.peerPort = peerPort
         try:
             self.conn.connect((peerIP, int(peerPort)))
             print("Connected")
         except:
-            close_connection()
+            self.close_connection()
             raise("Could not connect to",peerIP,peerPort)
+
+    # Find who we are supposed to be connected to and connect to them
+    def set_true_connection(self, key):
+        TO = self.trueOwner(key).split(':')
+        self.set_connection(TO[0], TO[1])
 
     # Close connection
     def close_connection(self):
-        try:
-            self.conn.close()
-        except:
-            raise("Couldn't close connection. Hard reset")
+        if self.conn is not None:
+            try:
+                self.conn.close()
+            except:
+                raise("Couldn't close connection. Hard reset")
         self.conn = None
         self.peerIP = None
         self.peerPort = None
 
     # Inserting value into DHT
     def insert(self, key, value):
+        self.set_true_connection(key)
+
         if self.conn is None:
             raise("Not connected to a peer")
         self.conn.send("INS".encode())
-        sendKey(self.conn, getHash(key))
+        sendKey(self.conn, self.prepKey(key))
+
         response1 = recvAll(self.conn, 1)
-        sendVal(value)
+        if response1.decode() == 'F':
+            return response1.decode()
+
+        sendVal(self.conn, self.prepVal(value))
+    
         response2 = recvAll(self.conn, 1)
+        return response2.decode()
 
     # Removing value from DHT
     def remove(self, key):
+        self.set_true_connection(key)
+
         if self.conn is None:
             raise("Not connected to a peer")
         self.conn.send("REM".encode())
-        sendKey(self.conn, getHash(key))
+        sendKey(self.conn, self.prepKey(key))
         response1 = recvAll(self.conn, 1)
+        return response1.decode()
 
     # Getting value from the DHT by key
     def get(self, key):
+        self.set_true_connection(key)
+
         if self.conn is None:
             raise("Not connected to a peer")
         self.conn.send("GET".encode())
-        sendKey(self.conn, getHash(key))
+        sendKey(self.conn, self.prepKey(key))
         response1 = recvAll(self.conn, 1)
         data = None
         if response1.decode() == 'T':
             data = recvVal(self.conn)
+        return (response1.decode(), data)
 
     # Checking for existence of a key
     def exists(self, key):
+        self.set_true_connection(key)
+
         if self.conn is None:
             raise("Not connected to a peer")
         self.conn.send("EXI".encode())
@@ -87,3 +124,38 @@ class DHTInterface:
         sendKey(self.conn, key_to_send)
         response1 = recvAll(self.conn, 1)
         return response1.decode()
+
+    # Finding true owner
+    def trueOwner(self, key):
+        candidate = self.owns(key) # First candidate to compare to
+        temp = candidate
+        returned_peer = ''
+        while candidate != returned_peer:
+            candidate = temp
+            _conn = socket(AF_INET, SOCK_STREAM)
+            connIP = candidate.split(':')[0]
+            connPort = int(candidate.split(':')[1])
+
+            _conn.connect((connIP, connPort))
+            _conn.send("OWN".encode())
+            sendKey(_conn, self.prepKey(key))
+
+            returned_peer = recvAddress(_conn)
+            returned_peer = returned_peer[0] + ":" + str(returned_peer[1])
+            temp = returned_peer
+            _conn.close()
+
+        return temp # If here, temp is the true owner
+
+    # Ask for who owns the space
+    def owns(self, key):
+        if self.conn is None:
+            raise("Not connected to a peer")
+        self.conn.send("OWN".encode())
+
+        key_to_send = self.prepKey(key)
+
+        sendKey(self.conn, key_to_send)
+        response1 = recvAddress(self.conn)
+        return response1[0] +":"+ str(response1[1])
+
